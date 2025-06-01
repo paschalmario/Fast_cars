0ae1fd362b84384515f154fe4102236a25a41b7b
Zikora <zikorapascal@gmail.com>
2025-06-01 19:34:40 +0100

    add flawless navigation

--- website/views.py
+++ website/views.py
@@ -4,4 +4,19 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", current_page='home')
+@views.route('/features')
+def features():
+    return render_template("features.html", current_page='Features')
+
+@views.route('/contact')
+def contact():
+    return render_template("contact.html", current_page='Contact')
+
+@views.route('/review')
+def review():
+    return render_template("review.html", current_page='Review')
+
+@views.route('/about')
+def about():
+    return render_template("about.html", current_page='about')
